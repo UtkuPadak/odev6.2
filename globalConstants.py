4036bd2e8e1ded83b857dc36811739659ccc384b
UtkuPadak <127566165+UtkuPadak@users.noreply.github.com>
2023-04-05 10:00:44 -0800

    Create globalConstants.py

---
+++ globalConstants.py
@@ -0,0 +1,5 @@
+#Tüm projeyi ilgilendiren değişkenleri burada tanımlayıp tek noktadan yönetelim.
+URL = "https://www.saucedemo.com/"
+product_list = []
+sorted_product_list = ["Sauce Labs Backpack","Sauce Labs Bike Light","Sauce Labs Bolt T-Shirt","Sauce Labs Fleece Jacket","Sauce Labs Onesie","Test.allTheThings() T-Shirt (Red)"]
+isimli_urun_listesi = []
